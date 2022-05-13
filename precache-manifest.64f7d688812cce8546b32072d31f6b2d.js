9c29a9d2faf7ea27d396f484908e0728939a27b8
Rishav <suranarishav08@gmail.com>
2022-05-13 23:49:06 +0530

    Updates

---
+++ precache-manifest.64f7d688812cce8546b32072d31f6b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ba186765d73452782c7",
+    "url": "/TextUtils-React/static/css/main.b13f7e7b.chunk.css"
+  },
+  {
+    "revision": "8ba186765d73452782c7",
+    "url": "/TextUtils-React/static/js/main.8ba18676.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils-React/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "728c46745d4df8a2440f543f3a8c7af3",
+    "url": "/TextUtils-React/index.html"
+  }
+];
